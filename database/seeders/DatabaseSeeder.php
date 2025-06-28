c6fd747fc118b12f3530f20435015a8621c07dd0
programminglives <tmg.sun-j@hotmail.com>
2025-06-28 16:45:22 +0545

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,7 +18,9 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@globalyhub.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
         ]);
     }
 }
