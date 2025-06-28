76d49e06f75334659bd9d1ea185ccc812978bdcc
programminglives <tmg.sun-j@hotmail.com>
2025-06-28 16:56:51 +0545

    feat:issue token to user

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ApiTokenController;
+
+Route::post('/sanctum/token', [ApiTokenController::class, 'issueToken']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
